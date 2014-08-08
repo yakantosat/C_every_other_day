8ffd0ecedfa1eaa1c0dd5a439db22112c552154f
yakantosat <yakantosat@gmail.com>
2014-08-08 10:16:50 +0800

    update scan.c

--- scan.c
+++ scan.c
@@ -4,14 +4,13 @@
 int look_for_the(char *);
 
 int main(void) {
-    char string[101];
+    char *string;
     int num = 0;
     
-    printf("Please input the string: \n");
-    fgets(string, 100, stdin);
+    string = "the cv iass  vy ba comp. the csdwe fw. the csds is sldkslsl the";
     num = look_for_the(string);
 
-    printf("\nThe number of 'the' in the string is: %d\n", num);
+    printf("The number of 'the' in the string is: %d\n", num);
 
     return 0;
 }
@@ -19,14 +18,12 @@ int main(void) {
 int look_for_the(char *string) {
     int n = 0, off;
 
-    char *tmp, *offset;
-    tmp = string;
-
-    offset = strstr(tmp, "the");
+    char *offset;
+    offset = strstr(string, "the");
 
     while (offset) {
         n++;
-	offset = strstr(offset, "the");
+	offset = strstr(offset + 3, "the");
     }
 
     return n;
