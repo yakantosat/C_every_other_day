0fdc52d15f07afecff4e193f8e94fcd108d63cd7
yakantosat <yakantosat@gmail.com>
2014-09-02 11:01:54 +0800

    update sum.c

--- sum.c
+++ sum.c
@@ -6,15 +6,19 @@ unsigned int sum(FILE *stream);
 
 int main(int argc, char **argv) {
     int i;
-    for (i = 1; i < argc; i++) {
-        FILE *fp;
-	fp = fopen(argv[i], "r");
-	if (fp == NULL) {
-	    perror(argv[i]);
-	    exit(EXIT_FAILURE);
-	}
+    if (argc <= 1) {
+        printf("%s: %u\n", "stdin", sum(stdin));
+    } else {
+        for (i = 1; i < argc; i++) {
+            FILE *fp;
+	    fp = fopen(argv[i], "r");
+	    if (fp == NULL) {
+	        perror(argv[i]);
+	        exit(EXIT_FAILURE);
+	    }
 
-	printf("%s: %u\n", argv[i], sum(fp));
+	    printf("%s: %u\n", argv[i], sum(fp));
+        }
     }
     return 0;
 }
