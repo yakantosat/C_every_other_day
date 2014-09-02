4d28de6dbe46eaad499fe114bca9c73824e77b14
yakantosat <yakantosat@gmail.com>
2014-09-02 10:44:11 +0800

    upload sum.c

---
+++ sum.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+unsigned int sum(FILE *stream);
+
+int main(int argc, char **argv) {
+    int i;
+    for (i = 1; i < argc; i++) {
+        FILE *fp;
+	fp = fopen(argv[i], "r");
+	if (fp == NULL) {
+	    perror(argv[i]);
+	    exit(EXIT_FAILURE);
+	}
+
+	printf("%s: %u\n", argv[i], sum(fp));
+    }
+    return 0;
+}
+
+unsigned int sum(FILE *stream) {
+    int c;
+    unsigned int summery = 0, seed = 18;
+    while ((c = fgetc(stream)) != EOF) {
+        summery += (c + seed);
+    }
+    fclose(stream);
+    return summery;
+}
